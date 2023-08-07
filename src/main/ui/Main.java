8dfd3ced04683f8c336831ad24153d5c5309af44
Hades <mmridul@students.cs.ubc.ca>
2023-08-06 20:26:53 -0700

    added image

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         try {
-            new CopyClass();
+            new ReadingListGUI();
         } catch (FileNotFoundException e) {
             System.out.println("Unable to start application: No file found");
         }
